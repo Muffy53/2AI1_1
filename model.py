7437d1b9b7bffe5212e48e3e00b9999ab8d8dbb1
Adyasa <adyasamaharana2828@gmail.com>
2026-03-20 22:04:33 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,13 +3,10 @@ from sklearn.preprocessing import LabelEncoder
 df=pd.read_csv('insurance_data_linear.csv')
 df.head()
 df.info()
-le=LabelEncoder()
-#This creates a tool that converts labels - numbers
 
-df['sex']=le.fit_transform(df['sex'])
-df['smoker']=le.fit_transform(df['smoker'])
-df['region']=le.fit_transform(df['region'])
+#This creates a tool that converts labels - numbers
+df = pd.get_dummies(df,columns = ['sex','smoker','region'], drop_first=True)
 
 # fit_transform(): learns categories and converts them into numbers
-
+print("Categorical variables encoded successsfully ")
 print(df.head())
